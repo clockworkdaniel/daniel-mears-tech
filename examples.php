d403f0a09311f891e065d15ae07680c2458aa294
Daniel Mears <clockworkdaniel@gmail.com>
2019-01-14 11:05:02 +0000

    new layout, comment out some for moment

--- examples.php
+++ examples.php
@@ -2,103 +2,102 @@
 
 <section class="content-area">
 	<div class="examples-content">
-		<p class="pull-out-copy">Examples of my work</p>
-		<p>This page is work in progress!<br>It currently lacks CSS grid fallback support, nice images, and a couple of examples</p>
+		<p class="pull-out-copy trigger-on-scroll" data-trigger-point="0.1">Examples of my work</p>
 		</ul>
-		<ul class="examples-list">
-			<li class="examples-item--large-left ">
-				<!-- <a class="examples-item__image" href="https://github.com/clockworkdaniel/daniel-mears-tech">
+		<ul class="examples__list">
+			<li class="examples__item">
+				<!-- <a class="examples__item-image react-songwriter-image" href="https://github.com/clockworkdaniel/react-songwriter">
 				</a> -->
-				<div class="examples-item__copy">
-					<h2 class="examples-item__title this-website-image">This Website!</h2>
-					<p>You probably know what it is because you're on it...</p>
-					<ul>
+				<div class="examples__item-copy">
+					<h2 class="examples__item-title trigger-on-scroll" data-trigger-point="0.1">React Songwriter</h2>
+					<p class="trigger-on-scroll" data-trigger-point="0.1">More than your average SPA todo list! In theory, the app would primarily serve songwriters, with the crux being that they are able to create new songs, write lyrics and 'paint' chords over the top of those lyrics.</p>
+					<p class="trigger-on-scroll" data-trigger-point="0.1">I've used this project to get practice at various JS Frameworks on both the server and client. Nonetheless, I should have a beta online soon!</p>
+					<ul class="trigger-on-scroll" data-trigger-point="0.1">
+						<li>React and Redux</li>
+						<li>RESTful API built with Express.js</li>
+						<li>MongoDB database with Mongoose ODM</li>
+						<li>Webpack</li>
+						<li>airBnb linting</li>
+					</ul>
+					<p class="examples__item-text-link trigger-on-scroll" data-trigger-point="0.1"><a href="https://github.com/clockworkdaniel/react-songwriter" title="React Songwriter">To the repo &rarr;</a></p>
+					<hr class="examples__item-hr trigger-on-scroll" data-trigger-point="0.1">
+				</div>
+			</li>
+			<li class="examples__item">
+				<!-- <a class="examples__item-image flamingo-game-image" data-iframe-trigger href="/examples/flamingo-game/index.php">
+				</a> -->
+				<div class="examples__item-copy">
+					<h2 class="examples__item-title trigger-on-scroll" data-trigger-point="0.1">Flamingo Game</h2>
+					<p class="trigger-on-scroll" data-trigger-point="0.1">My first foray into game development, and I'm pretty pleased with the results.</p>
+					<ul class="trigger-on-scroll" data-trigger-point="0.1">
+						<li>Vanilla JS</li>
+						<li>Canvas API</li>
+						<li>Touch Events API</li>
+					</ul>
+					<p class="examples__item-text-link trigger-on-scroll" data-trigger-point="0.1"><a href="/examples/flamingo-game/index.php" title="Flamingo Game" data-iframe-trigger >See this example &rarr;</a></p>
+					<hr class="examples__item-hr trigger-on-scroll" data-trigger-point="0.1">
+				</div>
+			</li>
+			<li class="examples__item">
+				<!-- <a class="examples__item-image" data-iframe-trigger href="/examples/amp-product-page/index.php">
+				</a> -->
+				<div class="examples__item-copy">
+					<h2 class="examples__item-title  trigger-on-scroll" data-trigger-point="0.1">AMP Product Page</h2>
+					<p class="trigger-on-scroll" data-trigger-point="0.1">AMP is more commonly used for articles, but Google are trying to push it into other industries.</p>
+					<ul class="trigger-on-scroll" data-trigger-point="0.1">
+						<li>Super fast!</li>
+						<li>Interactivity through amp-bind rather than JS</li>
+						<li>Stripped down HTML/CSS</li>
+					</ul>
+					<p class="examples__item-text-link trigger-on-scroll" data-trigger-point="0.1"><a href="/examples/amp-product-page/index.php" title="AMP Product Page" data-iframe-trigger >See this example &rarr;</a></p>
+					<hr class="examples__item-hr trigger-on-scroll" data-trigger-point="0.1">
+				</div>
+			</li>
+			<li class="examples__item">
+				<!-- <a class="examples__item-image" href="https://github.com/clockworkdaniel/daniel-mears-tech">
+				</a> -->
+				<div class="examples__item-copy">
+					<h2 class="examples__item-title trigger-on-scroll" data-trigger-point="0.1">This Website!</h2>
+					<p class="trigger-on-scroll" data-trigger-point="0.1">You probably know what it is because you're on it...</p>
+					<ul class="trigger-on-scroll" data-trigger-point="0.1">
 						<li>GSAP Javascript Animation</li>
 						<li>History Manipulation with History API</li>
 						<li>JS Promises</li>
-						<li>Custom Vanilla JS Scroll Handling</li>
-						<li>Built using SASS, following a CSS methodology akin to BEM</li>
-						<!-- <li>CSS Grid (on this page)</li> -->
+						<li>Custom JS Scroll Handling</li>
+						<li>SASS</li>
 					</ul>
-					<p class="examples-item__text-link"><a href="https://github.com/clockworkdaniel/daniel-mears-tech" title="My GitHub" >To my repo &rarr;</a></p>
+					<p class="examples__item-text-link trigger-on-scroll" data-trigger-point="0.1"><a href="https://github.com/clockworkdaniel/daniel-mears-tech" title="My GitHub" >To the repo &rarr;</a></p>
 				</div>
 			</li>
-			<li class="examples-item--small--long-copy ">
-				<!-- <a class="examples-item__image" data-iframe-trigger >
-				</a> -->
-				<div class="examples-item__copy">
-					<h2 class="examples-item__title swipeable-cards-image">Swipeable Cards</h2>
+			<!-- <li class="examples__item trigger-on-scroll" data-trigger-point="0.1">
+				<a class="examples__item-image" data-iframe-trigger >
+				</a>
+				<div class="examples__item-copy">
+					<h2 class="examples__item-title swipeable-cards-image">Swipeable Cards</h2>
 					<p>Swiped items are compiled for into a shortlist, while the user is presented with more of the sorts of items they are liking.</p>
 					<ul>
 						<li>Touch Events API</li>
 						<li>GSAP Javascript Animations</li>
 						<li>AJAX</li>
 					</ul>
-					<p class="examples-item__text-link"><a>This example will be up soon!</a></p>
+					<p class="examples__item-text-link"><a>This example will be up soon!</a></p>
 				</div>
-			</li>
-			<li class="examples-item--small--long-copy ">
-				<!-- <a class="examples-item__image" data-iframe-trigger href="/examples/amp-product-page/index.php">
-				</a> -->
-				<div class="examples-item__copy">
-					<h2 class="examples-item__title amp-product-page-image">AMP Product Page</h2>
-					<p>AMP is more commonly used for articles, but Google are trying to push it into other industries.</p>
-					<ul>
-						<li>Super fast!</li>
-						<li>Interactivity through amp-bind rather than JS</li>
-						<li>Stripped down HTML/CSS</li>
-					</ul>
-					<p class="examples-item__text-link"><a href="/examples/amp-product-page/index.php" title="AMP Product Page" data-iframe-trigger >See this example &rarr;</a></p>
-				</div>
-			</li>
-			<li class="examples-item--large-right ">
-				<!-- <a class="examples-item__image react-songwriter-image" data-iframe-trigger href="/examples/react-songwriter/index.php">
-				</a> -->
-				<div class="examples-item__copy">
-					<h2 class="examples-item__title">React Songwriter</h2>
-					<p>More than your average SPA todo list! The idea is that you write your lyrics, switch to chord mode and assign chords on a line/word/letter basis.</p>
-					<p>I actually started this with Angular 1, later ported what I had to React, and then switched my state management to Redux.</p>
-					<ul>
-						<li>Built with React.js</li>
-						<li>Expanded/Rebuilt with Redux for state management</li>
-						<li>Only optimized on desktop for the moment</li>
-					</ul>
-					<p class="examples-item__text-link"><a href="/examples/react-songwriter/index.php" title="React Songwriter" data-iframe-trigger >See this example &rarr;</a></p>
-				</div>
-			</li>
-			<li class="examples-item--small ">
-				<!-- <a class="examples-item__image flamingo-game-image" data-iframe-trigger href="/examples/flamingo-game/index.php">
-				</a> -->
-				<div class="examples-item__copy">
-					<h2 class="examples-item__title">Flamingo Game</h2>
-					<p>My first foray into game development, and I'm pretty pleased with the results.</p>
-					<ul>
-						<li>Vanilla JS</li>
-						<li>Canvas API</li>
-						<li>Touch Events API</li>
-					</ul>
-					<p class="examples-item__text-link"><a href="/examples/flamingo-game/index.php" title="Flamingo Game" data-iframe-trigger >See this example &rarr;</a></p>
-				</div>
-			</li>
-			<li class="examples-item--small ">
-				<!-- <a class="examples-item__image warriors-quiz-image" data-iframe-trigger >
-				</a> -->
-				<div class="examples-item__copy">
-					<h2 class="examples-item__title">Warriors Quiz</h2>
+			</li> -->
+			<!-- <li class="examples__item trigger-on-scroll" data-trigger-point="0.1">
+				<a class="examples__item-image warriors-quiz-image" data-iframe-trigger >
+				</a>
+				<div class="examples__item-copy">
+					<h2 class="examples__item-title">Warriors Quiz</h2>
 					<p>Choose from shuffled answer&hellip; I'm sure you've completed your fair share on buzzfeed ;)</p>
 					<ul>
 						<li>Shuffled answers</li>
 						<li>Velocity.js animation</li>
 						<li>Originally used Facebook API</li>
 					</ul>
-					<p class="examples-item__text-link"><a>This example will be up soon!</a></p>
+					<p class="examples__item-text-link"><a>This example will be up soon!</a></p>
 				</div>
-			</li>	
+			</li> -->
 		</ul>
-	</div>
-	<div class="examples-iframe__container">
-		<div class="examples-iframe__close">&#215;</div>
-		<iframe class="examples-iframe" src=""></iframe>
 	</div>
 </section>
 

0bf2cc9d7e649cf8e9fbcff1849fb4b1821ab029
Daniel Mears <clockworkdaniel@gmail.com>
2019-01-14 11:05:35 +0000

    utility iframe in footer

--- footer.php
+++ footer.php
@@ -1,2 +1,6 @@
-    </body>
+		<div class="examples-iframe__container">
+			<div class="examples-iframe__close">&#215;</div>
+			<iframe class="examples-iframe" src=""></iframe>
+		</div>
+	</body>
 </html>

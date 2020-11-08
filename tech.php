e5b302458d59fc184708e6318e14462ffcd8772e
clockworkdaniel <clockworkdaniel@gmail.com>
2020-11-08 14:29:14 +0000

    update tech copy

--- tech.php
+++ tech.php
@@ -7,52 +7,64 @@
 		<ul class="tech-list">
 			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">Javascript</h2>
-				<p class="tech-list__copy">It's good stuff and getting better all the time. I never could have predicted my fervour for destructuring, and I'm always trying to push my usage of ES6+'s async options.</p>
+				<p class="tech-list__copy">It's good stuff and getting better all the time.</p>
+			</li>
+      <li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
+				<h2 class="tech-list__heading">Typescript</h2>
+				<p class="tech-list__copy">I started using typescript in 2019, and the more I've used it, the more I'm inclined to sing its praises. I'd go as far to say it's absolutely essential for any substantial javascript application.</p>
 			</li>
 			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">CSS</h2>
-				<p class="tech-list__copy">Writing css is my bread and butter. What most excites me is the advance of CSS grid, but that's not to say devs should overlook backwards compatability. Speaking of which, I've learnt the hard way that no, IE11 does not have proper flexbox support.</p>
+				<p class="tech-list__copy">What excites me most is the advance of CSS grid, I've actually made a <a href="https://youtu.be/6HcWSfioYx4">presentation on the subject</a>. I've learnt the hard way that no, IE11 does not have proper flexbox support.</p>
 			</li>
 			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">CSS Architecture</h2>
-				<p class="tech-list__copy">I mention this seperately because no matter how fancy your CSS is, you need make sure it's gonna play nicely at scale. Personally I've used a lot of BEM, but I don't think that's the only way to go about things.</p>
+				<p class="tech-list__copy">I mention this seperately because no matter how fancy your CSS is, you need make sure it's gonna play nicely at scale. Some CSS-in-JS libraries are a great help, but there are always questions about what library or methodology suits a particular project.</p>
 			</li>
 			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">React.js</h2>
-				<p class="tech-list__copy">Good results, great workflow, but best of all it encourages good practices. By contrast, I have a distant memory of using Angular 1x and I'm fairly sure its a traumatic one.</p>
+				<p class="tech-list__copy">Good results and a nice declarative workflow. I'm a big fan of hooks and context, the latter of which can reduce the necessity for Redux.</p>
 			</li>
 			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">Redux</h2>
-				<p class="tech-list__copy">Brilliant, deliberate state management.</p>
+				<p class="tech-list__copy">Brilliant, deliberate state management. The downside is excessive boilerplate, especially when it is implemented with typescript, but the trade still seems a good one.</p>
 			</li>
 			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">DevTools</h2>
 				<p class="tech-list__copy">Basically the swiss army knife of front-end development, perfect for debugging and performance optimisation.</p>
 			</li>
-			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
+			<!-- <li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">Web Animation</h2>
 				<p class="tech-list__copy">I want to be doing more of it. A couple of years ago I put a fair chunk of time into velocity.js, but more recently I've been using GSAP.</p>
+			</li> -->
+      <li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
+				<h2 class="tech-list__heading">Testing</h2>
+				<p class="tech-list__copy">I have a slight preference towards integration testing than unit testing, but that is not to say they are mutually exclusive. Well written tests are an excellent place to provide clarity around expected behaviours, and introduce developers to new areas of code.</p>
+			</li>
+      <li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
+				<h2 class="tech-list__heading">RXJS</h2>
+				<p class="tech-list__copy">A very interesting programming library that, in my experience, both frustrates and delights. What I would say is that RXJS shines if it is used in the right situation.</p>
 			</li>
 			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">Express.js</h2>
-				<p class="tech-list__copy">Recently I've been using Express.js to develop a RESTful API to serve a songwriting web React application.</p>
+				<p class="tech-list__copy">A solid way to write serverside JS.</p>
 			</li>
-			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
+			<!-- <li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">Mongoose</h2>
 				<p class="tech-list__copy">In the same web app mentioned above, I have been using Mongoose for data modeling.</p>
-			</li>
+			</li> -->
 			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">PWA</h2>
-				<p class="tech-list__copy">Or Progressive Web Applications. For the record, I DON'T have experience using the service worker API yet&hellip; Nevertheless, having read quite a bit about them, it seems to me that service workers point to is the future where web and native apps converge.</p>
+				<p class="tech-list__copy">Or Progressive Web Applications. I don't have experience using the service worker API yet&hellip; Nevertheless, having read quite a bit about them, it seems to me that service workers point to is the future where web and native apps converge.</p>
 			</li>
-			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
+			<!-- <li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">AMP</h2>
 				<p class="tech-list__copy">Or Accelerated Mobile Pages. It turns out it's pretty fun stripping out all the fluff to make a super-fast page. Also I was surprised with how much interactivity you can achieve with it's simple state management system.</p>
-			</li>
-			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
+			</li> -->
+			<!-- <li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">Visual Studio Code</h2>
 				<p class="tech-list__copy">I just love it, it's so handy without being overly complex. Integrated terminal!</p>
-			</li>
+			</li> -->
 			<li class="tech-list__item trigger-on-scroll" data-trigger-point="0.1">
 				<h2 class="tech-list__heading">Game Dev</h2>
 				<p class="tech-list__copy">Tried my hand at this for the first time last year, using the Canvas API. A lot of fun.</p>
